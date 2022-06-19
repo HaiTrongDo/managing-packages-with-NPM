240ada68446813c0abd3cf44203356d5e1f83e25
HaiTrongDo <dotronghai91@gmail.com>
2022-06-19 21:48:46 +0700

    Meet the Node Console

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let app = express();
+var bGround = require('fcc-express-bground');
+
 
 //Modify the myApp.js file to log "Hello World" to the console.
-console.log("Hello World");
+bGround.log("Hello World");
 
 
 
