15e8c930e4999a5d7de22940aa7192572d231a5a
HaiTrongDo <dotronghai91@gmail.com>
2022-06-19 22:43:41 +0700

    Start a Working Express Server

--- server.js
+++ server.js
@@ -55,6 +55,11 @@ if (!process.env.DISABLE_XORIGIN) {
 //   }
 // })
 
+ app.get("/",(req, res) => {
+     res.send("Hello Express");
+ });
+
+
 //Listen on port set in environment variable or default to 3000
  var port = process.env.PORT || 3000;
  bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
